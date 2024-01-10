f78f1b294902641e011284430bfd6411a1319c41
Arpita-jain19 <jainarpita56@gmail.com>
2024-01-10 13:38:42 +0530

    Adding arrayList.java

---
+++ DemoArrayList.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+public class DemoArrayList {
+    
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList();
+        DemoArrayList ad=new DemoArrayList();
+        System.out.println(ad);
+        arrayList.add(79);
+        arrayList.add(8);
+        arrayList.add("Hello");
+          arrayList.add("Hello");
+            arrayList.add("Hello");
+              arrayList.add("Hello");
+                arrayList.add("Hello");
+                  arrayList.add("Hello");
+                    arrayList.add("Hello");
+                    arrayList.add("Hello");
+                arrayList.add("Hello");
+                  arrayList.add("Hello");
+                    arrayList.add("Hello");
+                      arrayList.add("Hello");
+                        arrayList.add("Hello");
+        arrayList.remove(0);
+        System.out.println(arrayList);
+
+    }
+    
+}
